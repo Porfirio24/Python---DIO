f8cacbc5a8dea2f5a47abc4465ebf72ef8d15c14
Porfirio-Neto <netinholeite2607@gmail.com>
2025-08-20 15:06:06 -0300

    Adicionando Repositrio

---
+++ 00_Fundamentos/Exercicios/Questão1.py
@@ -0,0 +1,15 @@
+"""
+imagine que você queira imprimir 'Aprovado(a)', caso o estudante tenha uma média superior ou igual a 7, e 'Reprovado(a)', caso a média seja inferior a 7
+"""
+
+# > Resolução
+
+media = float(input("Digite sua Media "))
+presença = int(input("Digite sua presença "))
+
+if media >= 7.0 and presença >= 50:
+  print("Você foi Aprovado(a) ")
+elif media >= 5.9:
+  print("Você foi para a Recuperação")
+else:
+  print("Você foi Reprovado(a)")
---
+++ 00_Fundamentos/Projeto/Banco.py
@@ -0,0 +1,78 @@
+saldo = 0
+limite = 500
+extrato = ""
+numero_saque = 0
+limite_saque = 3
+
+while True:
+    print("""
+          ==== Bem Vindo ao Pybank ====
+          [0] Entrar
+          [1] Sair
+          =============================
+          """)
+    opcao = input("Digite uma opção: ")
+
+    if opcao == "0":
+        while True:
+            print("""
+            ====== MENU ======
+
+            [2] Deposito 
+            [3] Saque
+            [4] Extrato
+            [5] Sair
+
+            ================
+            """)
+            opcao = input("Escolha uma Opção: ")
+
+            if opcao == "2":
+                valor = float(input("Qual o valor deseja depositar: "))
+                if valor > 0:
+                    saldo += valor
+                    extrato += f"Depósito R$: {valor:.2f}\n"
+                    print("Depósito Realizado com Sucesso!")
+                else:
+                    print("Erro ao Depositar")
+
+            elif opcao == "3":
+                valor = float(input("Digite o valor do Saque: "))
+
+                excedeu_saldo = valor > saldo
+                excedeu_limite = valor > limite
+                excedeu_saque = numero_saque >= limite_saque
+
+                if excedeu_saldo:
+                    print("Saldo Insuficiente!")
+                elif excedeu_limite:
+                    print(f"O limite de saque é de R${limite:.2f}")
+                elif excedeu_saque:
+                    print("Número máximo de saques já realizado!")
+                elif valor > 0:
+                    saldo -= valor
+                    extrato += f"Saque R$: {valor:.2f}\n"
+                    numero_saque += 1
+                    print("Saque Realizado com Sucesso!")
+                else:
+                    print("Valor de Saque Inválido!")
+
+            elif opcao == "4":
+                print("==== Extrato ====")
+                print("Não houve movimentação na sua Conta!" if not extrato else extrato)
+                print(f"Saldo: R${saldo:.2f}\n")
+                print("=================")
+
+            elif opcao == "5":
+                print("Obrigado por sua Preferência!")
+                break
+
+            else:
+                print("Escolha uma opção Válida!")
+
+    elif opcao == "1":
+        print("Saindo do PyBank... Obrigado por usar nossos serviços!")
+        break
+
+    else:
+        print("Escolha uma opção válida!")
